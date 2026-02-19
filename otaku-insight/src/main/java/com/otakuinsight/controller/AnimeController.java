eb2ac53d53eb70792b8c264b6ddb3aac0c1c1b50
vineet081 <vineethsoudri01@gmail.com>
2026-02-19 19:53:13 +0530

    cors configured 2

--- otaku-insight/src/main/java/com/otakuinsight/controller/AnimeController.java
+++ otaku-insight/src/main/java/com/otakuinsight/controller/AnimeController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://your-vercel-app.vercel.app")
 @RestController
 @RequestMapping("/api/anime")
 public class AnimeController {
